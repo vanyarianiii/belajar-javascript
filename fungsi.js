51120a0378753d52324c5dc8865fd017c763d8b6
vanyarianiii <siti87rizkiani@gmail.com>
2024-01-29 13:13:55 +0700

    hii

---
+++ fungsi.js
@@ -0,0 +1,19 @@
+var x ="25";
+var y ="3.14";
+
+var toNum = parseInt(x);
+var toFlat = parseFloat(y);
+ 
+function jumlah;(a, b) {
+   return a + b;
+
+}
+
+var kurang = function (a, b) {
+    return a - b;
+}
+
+alert(jumlah(5, 20));
+alert(kurang(25, 5));
+
+
