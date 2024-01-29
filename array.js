f8295e26aee19a486e1b7f3fbd899c7da98092dc
vanyarianiii <siti87rizkiani@gmail.com>
2024-01-29 13:38:54 +0700

    haii

---
+++ array.js
@@ -0,0 +1,8 @@
+// var namaarray = new Array()
+var lemari = new Array("dompet", 50000, true);
+console.log(lemari[1]);
+console.log(lemari[2]);
+
+lemari[3]  = "ktp";
+lemari.push("ATM");
+console.log(lemari);
